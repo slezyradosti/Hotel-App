7c1bc6db1556137d34fddb7e394a7361dcb6fb87
Life is squid <rredzer000@gmail.com>
2024-12-18 23:04:23 +0000

    add Hotels controller

---
+++ Hotel.API/Controllers/HotelsController.cs
@@ -0,0 +1,37 @@
+﻿using Hotel.Application.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Hotel.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HotelsController : ControllerBase
+    {
+        private readonly IHotelService _hotelService;
+
+        public HotelsController(IHotelService hotelService)
+        {
+            _hotelService = hotelService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllHotels()
+        {
+            try
+            {
+                var hotels = await _hotelService.GetAllHotelsAsync();
+
+                if (hotels is null) 
+                {
+                    return NotFound("No hotels found");
+                }
+
+                return Ok(hotels);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, new { ex.Message });
+            }
+        }
+    }
+}
