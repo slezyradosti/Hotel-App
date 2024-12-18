889bac246f180b2985efec68126e943fc063efad
Life is squid <rredzer000@gmail.com>
2024-12-18 21:55:01 +0000

    add entities

--- Hotel.Domain/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Hotel.Domain
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ Hotel.Domain/Hotels/Hotel.cs
@@ -0,0 +1,17 @@
+﻿using Hotel.Domain.Rooms;
+
+namespace Hotel.Domain.Hotels
+{
+    public class Hotel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public float Rating { get; set; }
+        public string ImageUrl { get; set; }
+        public IEnumerable<DateTime> DatesOfTravel { get; set; }
+        public string BoardBasis { get; set; }
+        public IEnumerable<Room> Rooms { get; set; }
+
+    }
+}
